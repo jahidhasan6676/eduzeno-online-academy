94a0ece5015cb27176dbbcef427b215201dbbd48
MD. Jahid Hasan <jh18186676@gmail.com>
2025-10-15 21:02:26 +0600

    project stucture and project demo routing setup

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-gray-300'>
+            <h2 className='text-xl font-medium py-20 text-center'>This is Eduzeno Online Academy Footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
